daa9cacffc177715fda3755a9863981f87b8282b
smori <shinichiro.mori.1983@gmail.com>
2022-08-23 18:54:03 +0900

    vscode.workspace.onDidChangeTextDocument()

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,10 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('The extension "helloworld" is now active!');
 
 	commands.register(context);
+
+	vscode.workspace.onDidChangeTextDocument((event) => {
+		console.log(event.document.languageId);
+	}, null, context.subscriptions);
 }
 
 export function deactivate() {}
